eaa39c9fff59feeff152fea383bf8c7467ef8aed
dlwhddksiq <dlwhddksiq@naver.com>
2022-02-10 13:03:04 +0900

    비밀번호 해쉬 다시 설정

--- src/main/java/com/memories/Service/UserService.java
+++ src/main/java/com/memories/Service/UserService.java
@@ -1,21 +1,29 @@
 package com.memories.Service;
 
 
+import com.memories.DataModel.Role;
 import com.memories.DataModel.User;
 import com.memories.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     @Autowired
     private UserRepository userRepository;
 
     @Transactional
     public void 회원가입(User user) {
+        String rawPassword = user.getPassword(); //1234 원문
+        String encPassword = encoder.encode(rawPassword); // 해쉬
+        user.setPassword(encPassword);
+        user.setRole(Role.USER);
         userRepository.save(user);
     }
 
