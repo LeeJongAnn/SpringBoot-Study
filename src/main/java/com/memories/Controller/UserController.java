d8418e8ce7ee99f2023adb6303c40a255de44906
dlwhddksiq <dlwhddksiq@naver.com>
2022-03-11 17:44:19 +0900

    페이지 상세보기 추가 및 리드미 수정

--- src/main/java/com/memories/Controller/UserController.java
+++ src/main/java/com/memories/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.memories.Controller;
 
 import com.memories.DataModel.Board;
+import com.memories.Repository.BoardRepository;
 import com.memories.Service.BoardService;
 import com.memories.auth.PrincipalDetail;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,13 +25,24 @@ public class UserController {
     @Autowired
     public BoardService boardService;
 
+    @Autowired
+    public BoardRepository boardRepository;
+
     @GetMapping("/")
     public String index(@AuthenticationPrincipal PrincipalDetail principal, Model model, @PageableDefault(size=3,sort="id",direction = Sort.Direction.DESC) Pageable pageable) {
         Page<Board> boardslist = boardService.글목록(pageable);
+
         System.out.println(boardslist);
         model.addAttribute("boards",boardslist);
         return "index";
     }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/detail";
+    }
+
 
     @GetMapping("/auth/login")
     public String login() {
